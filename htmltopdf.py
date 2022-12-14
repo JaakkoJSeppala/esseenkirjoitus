5f6de8f72455ef8f687cfde1b0ab3364e1f3b4f6
Jaakko Seppälä <jaakko.j.seppala@gmail.com>
2022-12-14 16:08:25 +0200

    Add files via upload

--- htmltopdf.py
+++ htmltopdf.py
@@ -2,11 +2,20 @@ import pdfkit
 import sys
 import fileinput
 import os
+from pdf2image import convert_from_path
+from PIL import Image
+import subprocess
 
 kurssikoodi = 'KTKP010'
 kurssinimi = 'Oppiminen ja ohjaus'
 
-replacements = {'ä':'&auml;', 'Ä':'&Auml;', 'Ö':'&ouml;', 'ö':'&ouml;', 'kurssikoodi':kurssikoodi, 'kurssinimi':kurssinimi}
+replacements={}
+replacements.update({'ä':'&auml;'})
+replacements.update({'Ä':'&Auml;'})
+replacements.update({'ö':'&ouml;'})
+replacements.update({'Ö':'&ouml;'})
+replacements.update({'kurssikoodi':kurssikoodi})
+replacements.update({'kurssinimi':kurssinimi})
 
 with open('koe.html') as infile, open('uusi.html', 'w') as outfile:
     for line in infile:
@@ -14,4 +23,5 @@ with open('koe.html') as infile, open('uusi.html', 'w') as outfile:
             line = line.replace(src, target)
         outfile.write(line)
 
-pdfkit.from_url('uusi.html','essee.pdf')
+command = 'wkhtmltopdf -L 30mm -R 30mm -T 25mm -B 25mm uusi.html ./test.pdf'
+subprocess.Popen(command,shell=True)
