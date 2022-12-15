bd35cb09d5d3095fef6c80505668d0eb365e9650
Jaakko Seppälä <jaakko.j.seppala@gmail.com>
2022-12-15 15:42:19 +0200

    Add files via upload

--- htmltopdf.py
+++ htmltopdf.py
@@ -1,13 +1,15 @@
 import signal
-import pdfkit
-import sys
-import fileinput
 import os
-from pdf2image import convert_from_path
-from PIL import Image
 import subprocess
 
-leftmargin= '30mm'
+
+def compile_document():
+    pro = subprocess.Popen(command, stdout=subprocess.PIPE,
+                           shell=True, preexec_fn=os.setsid)
+    os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
+
+
+leftmargin = '30mm'
 rightmargin = '30mm'
 topmargin = '25mm'
 bottommargin = '25mm'
@@ -15,13 +17,13 @@ bottommargin = '25mm'
 kurssikoodi = 'KTKP010'
 kurssinimi = 'Oppiminen ja ohjaus'
 
-replacements={}
-replacements.update({'ä':'&auml;'})
-replacements.update({'Ä':'&Auml;'})
-replacements.update({'ö':'&ouml;'})
-replacements.update({'Ö':'&ouml;'})
-replacements.update({'kurssikoodi':kurssikoodi})
-replacements.update({'kurssinimi':kurssinimi})
+replacements = {}
+replacements.update({'ä': '&auml;'})
+replacements.update({'Ä': '&Auml;'})
+replacements.update({'ö': '&ouml;'})
+replacements.update({'Ö': '&ouml;'})
+replacements.update({'kurssikoodi': kurssikoodi})
+replacements.update({'kurssinimi': kurssinimi})
 
 with open('koe.html') as infile, open('uusi.html', 'w') as outfile:
     for line in infile:
@@ -29,6 +31,9 @@ with open('koe.html') as infile, open('uusi.html', 'w') as outfile:
             line = line.replace(src, target)
         outfile.write(line)
 
-command = f'wkhtmltopdf -L {leftmargin} -R {rightmargin} -T {topmargin} -B {bottommargin} uusi.html ./test.pdf'
-pro = subprocess.Popen(command,stdout=subprocess.PIPE,shell=True, preexec_fn=os.setsid)
-os.killpg(os.getpgid(pro.pid), signal.SIGTERM)  
+command = f'wkhtmltopdf -L {leftmargin}'
+command += '-R {rightmargin}'
+command += '-T {topmargin}'
+command += '-B {bottommargin}'
+command += ' uusi.html ./test.pdf'
+compile_document(command)
